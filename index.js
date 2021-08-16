2def36994c5ec3783df6cf95f9366e5aba862dd1
Yaneizy Castillo <yccastillo18@gmail.com>
2021-08-15 20:06:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+
+//////////////////// SOLUTION ON GITHUB ///////////////
+// function findMatching(source, sought) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase() === sought.toLowerCase()
+//     )
+//   }
+// // const findMatching = drivers.filter(drivers => drivers.length 
+//     === 'Bobby', 'Sammy');
+
+// console.log(findMatching);
+
+
+//// why use .toLowerCase()???
+function findMatching(drivers, string) {
+    return drivers.filter(function(y) {
+        return y.toLowerCase().indexOf(string.toLowerCase()) !== -1})
+}
+
+////////// DON'T HAVE TO CONSOLE.LOG() //////////////
+// console.log(findMatching(drivers, 'Bobby'));
+// console.log(findMatching(drivers, 'Sammy'));
+
+///// what does it mean by querying the array????? like uhhhh?? 
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(function(x) {
+        return x.toLowerCase().indexOf(query.toLowerCase()) === 0}
+    )  
+}
+
+
+function matchName(drivers, obj) {
+    return drivers.filter(drivers => drivers.name === obj);
+}
